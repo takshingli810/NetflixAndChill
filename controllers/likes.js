f564e38ce328dfab7fe0e5d7975493c94f648a96
nreddy216 <nreddy216@gmail.com>
2016-03-13 12:24:47 -0700

    fixed models

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,7 +1,9 @@
-var Like = {
-  title: String,
-  imageUrl: String,
-  plot: String,
-  omdbURL: String,
-  yearOfRelease: String
-}
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Like', {
+    title: String,
+    imageUrl: String,
+    plot: String,
+    omdbURL: String,
+    yearOfRelease: String
+});
