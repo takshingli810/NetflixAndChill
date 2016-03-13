1ed5d69946d7a4a976bafc0c9c53d40370a38694
nreddy216 <nreddy216@gmail.com>
2016-03-13 12:59:05 -0700

    ACTUALLY FIXED LIKE

--- controllers/likes.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Like', {
-    title: String,
-    imageUrl: String,
-    plot: String,
-    omdbURL: String,
-    yearOfRelease: String
-});
