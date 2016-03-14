8ed92c2abbac56cfc2343bef53438a3ea198bdb3
takshingli810 <takshingli810@gmail.com>
2016-03-14 14:13:23 -0700

    fixed user.js

--- models/user.js
+++ models/user.js
@@ -1,23 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
-module.exports = mongoose.model('User',{
-  fb: {
-    id: String,
-    access_token: String,
-    firstName: String,
-    lastName: String,
-    email: String,
-    gender: String,
-    birthdate: Date,
-    sexualPref: String,
-    location: String,
-    profilePic: String,
-    password_digest: String
-  }
-});
-
 var Like = require("./like");
 
 //STILL UNSURE ABOUT WHERE THIS REFERENCED DATA WORKS
@@ -37,17 +20,6 @@ var UserSchema = new Schema({
   }]
 });
 
+var User = mongoose.model('User', UserSchema);
 
-
-// var User = mongoose.model('User', UserSchema);
-
-// module.exports = User;
-
-// Close connection on close
-// process.on('exit', function() {
-//   console.log('About to exit...');
-//   mongoose.disconnect(function() {
-//     console.log("Disconnected DB");
-//     process.exit(); // now exit the node app
-//   });
-// });
+module.exports = User;
